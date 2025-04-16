a8525e42bd3c0dd4604b5cc1e2c4d61a80ff669e
dima <dimasowner7201@gmail.com>
2025-04-16 16:24:25 +0300

    added unit tests for HabitManagementDtoMapper

---
+++ service/src/test/java/greencity/mapping/HabitManagementDtoMapperTest.java
@@ -0,0 +1,54 @@
+package greencity.mapping;
+
+import greencity.ModelUtils;
+import greencity.dto.habit.HabitManagementDto;
+import greencity.entity.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class HabitManagementDtoMapperTest {
+    private final HabitManagementDtoMapper mapper = new HabitManagementDtoMapper();
+
+    @Test
+    void convertTest() {
+        Language language = ModelUtils.getLanguage();
+        Tag tag = ModelUtils.getTag();
+        ShoppingListItem shoppingListItem = ModelUtils.getShoppingListItem();
+        HabitTranslation habitTranslation = HabitTranslation.builder()
+                .language(language)
+                .description("test")
+                .habitItem("test")
+                .name("test")
+                .build();
+
+        Habit habit = Habit.builder()
+                .id(1L)
+                .image("test")
+                .defaultDuration(1)
+                .complexity(1)
+                .tags(Set.of(tag))
+                .habitTranslations(List.of(habitTranslation))
+                .shoppingListItems(Set.of(shoppingListItem))
+                .build();
+
+        HabitManagementDto result = mapper.convert(habit);
+
+        assertNotNull(result);
+        assertEquals(habit.getId(), result.getId());
+        assertEquals(habit.getImage(), result.getImage());
+        assertEquals(habit.getDefaultDuration(), result.getDefaultDuration());
+        assertEquals(habit.getComplexity(), result.getComplexity());
+    }
+
+    @Test
+    void testHandleNullInput() {
+        assertThrows(NullPointerException.class, () -> mapper.convert((Habit) null));
+    }
+
+}
