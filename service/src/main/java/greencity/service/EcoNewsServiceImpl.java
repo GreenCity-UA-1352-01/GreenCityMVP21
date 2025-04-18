12ff1660d780e0ef3ae3e324995f6e4e919b8afe
Pavel_Chernyavckii <Chernyavckii15@gmail.com>
2025-04-19 01:31:40 +0300

    Fix: Return 403 Forbidden when updating eco news without permission

--- service/src/main/java/greencity/service/EcoNewsServiceImpl.java
+++ service/src/main/java/greencity/service/EcoNewsServiceImpl.java
@@ -489,7 +489,7 @@ public class EcoNewsServiceImpl implements EcoNewsService {
     public EcoNewsGenericDto update(UpdateEcoNewsDto updateEcoNewsDto, MultipartFile image, UserVO user) {
         EcoNews toUpdate = modelMapper.map(findById(updateEcoNewsDto.getId()), EcoNews.class);
         if (user.getRole() != Role.ROLE_ADMIN && !user.getId().equals(toUpdate.getAuthor().getId())) {
-            throw new BadRequestException(ErrorMessage.USER_HAS_NO_PERMISSION);
+            throw new AccessDeniedException(ErrorMessage.USER_HAS_NO_PERMISSION);
         }
         enhanceWithNewData(toUpdate, updateEcoNewsDto, image);
         ecoNewsRepo.save(toUpdate);
