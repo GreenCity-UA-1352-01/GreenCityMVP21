84778ceaa00827c28744e7c6b2f44ee219200b62
Rostyslav Zadyraichuk <rostykzadyraichuk010@gmail.com>
2025-04-19 00:24:01 +0300

    [31] fix 500 exception when null image is sent

--- core/src/main/java/greencity/validator/ImageValidator.java
+++ core/src/main/java/greencity/validator/ImageValidator.java
@@ -18,7 +18,7 @@ public class ImageValidator implements ConstraintValidator<ImageValidation, Mult
     @Override
     public boolean isValid(MultipartFile image, ConstraintValidatorContext constraintValidatorContext) {
         if (image == null) {
-            return true;
+            return false;
         } else {
             return validType.contains(image.getContentType());
         }
