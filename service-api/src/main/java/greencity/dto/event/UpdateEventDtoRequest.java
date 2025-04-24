536c00b24cb5a9f61cbea8f617299fd97fdb4cc3
Rostyslav Kushpit <rostyslavkushpit@gmail.com>
2025-04-25 02:22:34 +0300

    Added request&response dtos for EventImage.class and EventDateTimeLocation.class. Need to check.

--- service-api/src/main/java/greencity/dto/event/UpdateEventDtoRequest.java
+++ service-api/src/main/java/greencity/dto/event/UpdateEventDtoRequest.java
@@ -1,4 +1,36 @@
 package greencity.dto.event;
 
+import greencity.dto.eventdatetime.EventDateTimeLocationRequestDto;
+import greencity.dto.eventimage.EventImageRequestDto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UpdateEventDtoRequest {
+
+    @NotBlank(message = "The title of event can not be empty")
+    @Size(max = 70, message = "The title of event can not be longer than 70 characters")
+    private String title;
+
+    @NotBlank(message = "The description of event can not be empty")
+    @Size(min = 20, max = 63206, message = "The description is too short or too long")
+    private String description;
+
+    @NotEmpty(message = "The sessions of event can not be empty")
+    @Valid
+    private List<EventDateTimeLocationRequestDto> dateTimes;
+
+    @Valid
+    private EventImageRequestDto mainImage;
+
+    @Size(max = 5, message = "Maximum number of images is 5")
+    @Valid
+    private List<EventImageRequestDto> eventImages;
 }
