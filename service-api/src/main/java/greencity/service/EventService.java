bc8d8c55f6ff262c95ad9bf52282a1683420bbfb
Pavel_Chernyavckii <Chernyavckii15@gmail.com>
2025-04-24 22:18:36 +0300

    added logic for editing events

--- service-api/src/main/java/greencity/service/EventService.java
+++ service-api/src/main/java/greencity/service/EventService.java
@@ -1,4 +1,22 @@
 package greencity.service;
 
+import greencity.dto.econews.EcoNewsDto;
+import greencity.dto.econews.EcoNewsVO;
+import greencity.dto.econews.UpdateEcoNewsDto;
+import greencity.dto.event.EditEventDto;
+import greencity.dto.event.EditEventDtoResponse;
+import greencity.dto.user.UserVO;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public interface EventService {
+
+    /**
+     * Method for updating Event.
+     *
+     * @param editEventDto - instance of {@link EditEventDto}.
+     * @return instance of {@link EditEventDtoResponse};=.
+     */
+    EditEventDtoResponse update(EditEventDto editEventDto, List<MultipartFile> images, UserVO user);
 }
