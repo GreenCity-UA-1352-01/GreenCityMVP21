0551a827a7e2c504007d669eafc3168aa1300427
Pavel_Chernyavckii <Chernyavckii15@gmail.com>
2025-04-24 22:18:36 +0300

    added logic for editing events

--- core/src/main/java/greencity/controller/EventController.java
+++ core/src/main/java/greencity/controller/EventController.java
@@ -1,16 +1,55 @@
 package greencity.controller;
 
+import greencity.annotations.CurrentUser;
+import greencity.annotations.ImageValidation;
+import greencity.dto.event.EditEventDto;
+import greencity.dto.event.EditEventDtoResponse;
+import greencity.dto.user.UserVO;
 import greencity.service.EventService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Validated
 @RestController
-@RequestMapping("/event")
+@RequestMapping("/events")
 @AllArgsConstructor
 public class EventController {
     private final EventService eventService;
+
+
+    @Operation(summary = "Update events")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Event successfully updated"),
+            @ApiResponse(responseCode = "400", description = "Invalid request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "Event not found")
+    })
+    @PutMapping(value = "/update", consumes = {MediaType.APPLICATION_JSON_VALUE,
+    MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity <EditEventDtoResponse> update(
+            @Valid @RequestPart EditEventDto editEventDto,
+            @Parameter(description = "Event images (JPG/PNG ≤ 10MB, max 5)")@ImageValidation
+            @RequestPart(required = false) List<MultipartFile> images,
+            @Parameter(hidden = true) @CurrentUser UserVO user){
+        return ResponseEntity.ok(eventService.update(editEventDto, images, user));
+
+
+    }
+
 
 }
--- service/src/main/java/greencity/service/EventServiceImpl.java
+++ service/src/main/java/greencity/service/EventServiceImpl.java
@@ -1,8 +1,14 @@
 package greencity.service;
 
+import greencity.dto.event.EditEventDto;
+import greencity.dto.event.EditEventDtoResponse;
+import greencity.dto.user.UserVO;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -10,4 +16,8 @@ public class EventServiceImpl implements EventService {
 
     private final ModelMapper modelMapper;
 
+    @Override
+    public EditEventDtoResponse update(EditEventDto editEventDto, List<MultipartFile> images, UserVO user) {
+        return null;
+    }
 }
