af06eeb9dcde278aa02d7c938b918a00671a608f
AnastasiaRakuta <anastasiarakuta.2001@gmail.com>
2025-04-25 20:00:54 +0300

    add friends table creation, add friend entity

---
+++ dao/src/main/java/greencity/entity/Friend.java
@@ -0,0 +1,30 @@
+package greencity.entity;
+
+import greencity.enums.FriendsStatus;
+import lombok.*;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "friends")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Friend {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "friend_id", nullable = false)
+    private User friend;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private FriendsStatus status;
+}
