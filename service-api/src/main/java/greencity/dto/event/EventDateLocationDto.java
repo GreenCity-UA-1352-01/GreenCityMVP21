ac823174583b193a99f7dbe0e6285f9256d87f6f
AnastasiaRakuta <anastasiarakuta.2001@gmail.com>
2025-04-30 16:37:07 +0300

    [97] add locations and online link validation

--- service-api/src/main/java/greencity/dto/event/EventDateLocationDto.java
+++ service-api/src/main/java/greencity/dto/event/EventDateLocationDto.java
@@ -1,6 +1,8 @@
 package greencity.dto.event;
 
+import greencity.annotations.ValidLocationOrOnlineLink;
 import greencity.annotations.ValidTimeRange;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 import java.time.ZonedDateTime;
@@ -13,10 +15,15 @@ import java.time.ZonedDateTime;
 @Builder
 @ToString
 @ValidTimeRange
+@ValidLocationOrOnlineLink
 public class EventDateLocationDto {
     private ZonedDateTime startDateTime;
     private ZonedDateTime endDateTime;
     private String location;
+    @Pattern(
+            regexp = "^(https?://).*",
+            message = "Please add a link to the event. The link must start with http(s)://"
+    )
     private String onlineLink;
     private boolean allDay;
 }
