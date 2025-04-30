347ae45732edbb76eb8c6467b7959824e71dfe6b
Rostyslav Zadyraichuk <rostykzadyraichuk010@gmail.com>
2025-04-30 15:16:42 +0300

    merge entities with liquibase files

--- dao/src/main/java/greencity/entity/NotificationCounter.java
+++ dao/src/main/java/greencity/entity/NotificationCounter.java
@@ -10,12 +10,18 @@ import lombok.*;
 @Getter
 @Setter
 @Builder
+@EqualsAndHashCode(exclude = {"user"})
+@ToString(exclude = {"user"})
 public class NotificationCounter {
     @Id
+    @Column(name = "user_id")
+    private Long userId;
+
     @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
-    @Column
+    @Column(name = "count_of_notifications")
     private Integer countOfNotifications;
 }
