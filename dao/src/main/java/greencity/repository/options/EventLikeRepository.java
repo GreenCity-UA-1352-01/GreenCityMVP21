26c6f6db88bcd0f24e65386c4ce240005182fc79
Rostyslav Kushpit <rostyslavkushpit@gmail.com>
2025-05-07 04:09:58 +0300

    Remove NotifyUser annotation from controller method because of implementing toggle logic for likes

---
+++ dao/src/main/java/greencity/repository/options/EventLikeRepository.java
@@ -0,0 +1,17 @@
+package greencity.repository.options;
+
+import greencity.entity.EventLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EventLikeRepository extends JpaRepository<EventLike, Long> {
+    boolean existsByEventIdAndUserId(Long eventId, Long userId);
+    Long countByEventId(Long eventId);
+    Optional<EventLike> findByEventIdAndUserId(Long eventId, Long userId);
+
+
+    void deleteByEventIdAndUserId(Long eventId, Long userId); // for unlike in the future
+}
