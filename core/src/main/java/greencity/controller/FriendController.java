144779008bd462065595773b36c3708063a3e75e
AnastasiaRakuta <anastasiarakuta.2001@gmail.com>
2025-05-08 16:46:19 +0300

    [115] add logic for accepting friend

--- core/src/main/java/greencity/controller/FriendController.java
+++ core/src/main/java/greencity/controller/FriendController.java
@@ -98,6 +98,18 @@ public class FriendController {
         friendService.addFriend(currentUser.getId(), friendId);
     }
 
+    @PatchMapping("/accept/{friendId}")
+    @Operation(summary = "Accept friend request")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
+            @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND),
+            @ApiResponse(responseCode = "400", description = HttpStatuses.BAD_REQUEST)
+    })
+    public void acceptFriendRequest(@PathVariable Long friendId,
+                                    @Parameter(hidden = true) @CurrentUser UserVO userVO) {
+        friendService.acceptFriendRequest(userVO.getId(), friendId);
+    }
+
     @Operation(summary = "Get all users friends")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = HttpStatuses.OK),
