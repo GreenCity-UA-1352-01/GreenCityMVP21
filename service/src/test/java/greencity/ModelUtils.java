78aba4c0fea820d280dffa521155b13d44fed6f9
Rostyslav Zadyraichuk <rostykzadyraichuk010@gmail.com>
2025-05-08 16:58:44 +0300

    small fix in ModelUtils

--- service/src/test/java/greencity/ModelUtils.java
+++ service/src/test/java/greencity/ModelUtils.java
@@ -524,8 +524,8 @@ public class ModelUtils {
     }
 
     public static UpdateEcoNewsDto getUpdateEcoNewsDto() {
-        return new UpdateEcoNewsDto(1L, "title", "text", "shortInfo", Collections.singletonList("tag"),
-                "image", "source");
+        return new UpdateEcoNewsDto("1", "title", "text", "shortInfo", Collections.singletonList("tag"),
+                "image", "source", "text");
     }
 
     public static SearchNewsDto getSearchNewsDto() {
