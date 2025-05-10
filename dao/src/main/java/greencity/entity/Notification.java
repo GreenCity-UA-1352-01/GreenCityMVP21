9755a00716b01de4671f646e162c0404e516aad9
Rostyslav Zadyraichuk <rostykzadyraichuk010@gmail.com>
2025-05-10 17:06:48 +0300

    [101] small adjustments

--- dao/src/main/java/greencity/entity/Notification.java
+++ dao/src/main/java/greencity/entity/Notification.java
@@ -3,7 +3,6 @@ package greencity.entity;
 import greencity.enums.NotificationStatus;
 import jakarta.persistence.*;
 import lombok.*;
-
 import java.time.ZonedDateTime;
 
 @Entity
@@ -23,15 +22,33 @@ public class Notification {
     @Column
     private String action;
 
+    /**
+     * Field represents the specific location in the codebase where the action
+     * triggering this notification occurred. This could be a class name and
+     * method name, an API endpoint, or any other identifiable code location
+     * relevant to the action.
+     */
     @Column(name = "action_source", nullable = false)
     private String actionSource;
 
+    /**
+     * Field represents the identifier of the specific object involved
+     * in the action that triggered this notification.
+     */
     @Column(name = "object_id", nullable = false)
     private Long objectId;
 
+    /**
+     * Field represents the specific entity class name
+     * where the action triggering this notification occurred.
+     */
     @Column(name = "object_name", nullable = false)
     private String objectName;
 
+    /**
+     * Field represents the hyperlink associated with the specific object
+     * involved in the action that triggered this notification.
+     */
     @Column(name = "object_link")
     private String objectLink;
 
--- service/src/main/java/greencity/notification/NotificationPublisher.java
+++ service/src/main/java/greencity/notification/NotificationPublisher.java
@@ -1,12 +1,15 @@
 package greencity.notification;
 
 import greencity.dto.notification.NotificationRequestDto;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Component
 @AllArgsConstructor
+@Validated
 public class NotificationPublisher {
     private final ApplicationEventPublisher publisher;
 
@@ -18,7 +21,7 @@ public class NotificationPublisher {
      * @see NotificationListener
      * @author Roman Diakov & Rostyslav Zadyraichuk
      */
-    public void publish(NotificationRequestDto event) {
+    public void publish(@Valid NotificationRequestDto event) {
         if (event == null) {
             throw new IllegalArgumentException("Notification event must not be null");
         }
